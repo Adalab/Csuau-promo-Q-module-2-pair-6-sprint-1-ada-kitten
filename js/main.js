33394435b47ca0a00aab2a399b4b0e793b5da1ab
csuau <csuaubou@gmail.com>
2022-06-27 11:09:32 +0200

    add conditionals

--- js/main.js
+++ js/main.js
@@ -4,52 +4,29 @@ const form = document.querySelector(".js-new-form");
 
 const listCats = document.querySelector(".js_list");
 
-const kittenImage1 = "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg";
-const kittenName1 = "Anastacio";
-const kittenDesc =
-  "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
-const kittenRace = "British Shorthair";
+const input_search_desc = document.querySelector('.js_in_search_desc');
+input_search_desc.value = 'cariñoso';
+const descrSearchText = input_search_desc.value;
 
-const kittenImage2 =
-  "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg";
+const kittenDesc1 = document.querySelector('.card_description1');
+const kittenDesc2 = document.querySelector('.card_description2');
+const kittenDesc3 = document.querySelector('.card_description3');
 
-const kittenName2 = "Fiona";
+if (kittenDesc1.includes(descrSearchText)) {
+  kittenDesc1.classList.remove('hidden');
+}
 
-const kittenImage3 =
-  "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg";
 
-const kittenName3 = "Cielo";
-
-const kittenOne = `<li>${kittenImage1} ${kittenName1} ${kittenDesc}</li>`;
-kittenOne.classList.add(
-  "card_img",
-  "card_title",
-  "card_race",
-  "card_description"
-);
-
-const kittenTwo = `<li class="card">
-<img class="card_img"
-    src="https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg"
-    alt="gatito" />
-<h3 class="card_title">Fiona</h3>
-<h4 class="card_race">British Shorthair</h4>
-<p class="card_description">
-    Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.
-    Es una maravilla acariciarle!
-</p>
-</li>`;
-
-const kittenThree = `<li class="card">
-<img class="card_img"
-    src="https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg"
-    alt="gatito" />
-<h3 class="card_title">Cielo</h3>
-<h4 class="card_race">British Shorthair</h4>
-<p class="card_description">
-    Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.
-    Es una maravilla acariciarle!
-</p>
-</li> `;
-
-listCats.innerHTML = kittenOne + kittenTwo + kittenThree;
+if (kittenDesc2.includes(descrSearchText)) {
+  if (kittenDesc2.classList.contains('hidden')) {
+    kittenDesc2.classList.remove('hidden');
+  }
+  else { kittenDesc2.classList.add('hidden') }
+}
+if (kittenDesc3.includes(descrSearchText)) {
+  if (kittenDesc3.classList.contains('hidden')
+  ) {
+    kittenDesc3.classList.remove('hidden');
+  }
+  else { kittenDesc3.classList.add('hidden') }
+}
