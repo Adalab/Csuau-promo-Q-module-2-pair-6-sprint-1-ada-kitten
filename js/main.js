f398df12e200a4a2efdb8cacf3785953ce0f341b
csuau <csuaubou@gmail.com>
2022-07-01 11:10:06 +0200

    add objects

--- js/main.js
+++ js/main.js
@@ -141,3 +141,32 @@ const filterKitten = (event) => {
 };
 
 buttonSearch.addEventListener("click", filterKitten);
+
+
+//ejemplo de un objeto con la información del primer gatito
+const kittenData_1 = {
+  image: 'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg',
+  name: 'Anastacio',
+  desc: 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!',
+  race: 'British Shorthair',
+};
+const kittenData_2 = {
+  image: "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg",
+  name: 'Fiona',
+  desc: 'Ruiseño,cariñoso, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!',
+  race: 'British Shorthair',
+};
+const kittenData_3 = {
+  image: "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg",
+  name: 'Cielo',
+  desc: 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!',
+  race: 'British Shorthair',
+};
+
+function renderKitten (kittenData) {
+  console.log('kittenData.image');
+
+}
+renderKitten(kittenData_1);
+
+
