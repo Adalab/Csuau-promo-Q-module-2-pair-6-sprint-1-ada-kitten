525dd45e0560088c18918c82d301c7b7fc7814a2
MartaRodriguezOnis <marta.ronis@gmail.com>
2022-07-04 11:07:14 +0200

    Add Array

--- js/main.js
+++ js/main.js
@@ -1,35 +1,35 @@
 "use strict";
 
-const form = document.querySelector(".js-new-form");
+// const form = document.querySelector(".js-new-form");
 
-const listCats = document.querySelector(".js_list");
+// const listCats = document.querySelector(".js_list");
 
-const input_search_desc = document.querySelector(".js_in_search_desc");
-/*input_search_desc.value = "cariñoso";
-const descrSearchText = input_search_desc.value;
-*/
-const descrSearchText = input_search_desc.value;
+// const input_search_desc = document.querySelector(".js_in_search_desc");
+// /*input_search_desc.value = "cariñoso";
+// const descrSearchText = input_search_desc.value;
+// */
+// const descrSearchText = input_search_desc.value;
 
-const kittenDesc1 = document.querySelector(".card_description1");
-const kittenDesc2 = document.querySelector(".card_description2");
-const kittenDesc3 = document.querySelector(".card_description3");
+// const kittenDesc1 = document.querySelector(".card_description1");
+// const kittenDesc2 = document.querySelector(".card_description2");
+// const kittenDesc3 = document.querySelector(".card_description3");
 
-if (kittenDesc1.innerText.includes(descrSearchText)) {
-  kittenDesc1.parentElement.classList.remove("hidden");
-} else {
-  kittenDesc1.parentElement.classList.add("hidden");
-}
+// if (kittenDesc1.innerText.includes(descrSearchText)) {
+//   kittenDesc1.parentElement.classList.remove("hidden");
+// } else {
+//   kittenDesc1.parentElement.classList.add("hidden");
+// }
 
-if (kittenDesc2.innerText.includes(descrSearchText)) {
-  kittenDesc2.parentElement.classList.remove("hidden");
-} else {
-  kittenDesc2.parentElement.classList.add("hidden");
-}
-if (kittenDesc3.innerText.includes(descrSearchText)) {
-  kittenDesc3.parentElement.classList.remove("hidden");
-} else {
-  kittenDesc3.parentElement.classList.add("hidden");
-}
+// if (kittenDesc2.innerText.includes(descrSearchText)) {
+//   kittenDesc2.parentElement.classList.remove("hidden");
+// } else {
+//   kittenDesc2.parentElement.classList.add("hidden");
+// }
+// if (kittenDesc3.innerText.includes(descrSearchText)) {
+//   kittenDesc3.parentElement.classList.remove("hidden");
+// } else {
+//   kittenDesc3.parentElement.classList.add("hidden");
+// }
 
 // const icon = document.querySelector(".fa-plus-circle");
 // const menuColl = document.querySelector(".js-new-form");
@@ -123,50 +123,100 @@ if (kittenDesc3.innerText.includes(descrSearchText)) {
 
 // buttonCancelForm.addEventListener("click", cancelNewKitten);
 
-const buttonSearch = document.querySelector(".js_buttonSearch");
+// const buttonSearch = document.querySelector(".js_buttonSearch");
+
+// let listElement = document.querySelector(".js-listElement");
+
+// const filterKitten = (event) => {
+//   event.preventDefault();
+//   if (kittenDesc1.includes(descrSearchText)) {
+//     listElement.innerHTML += kittenOne;
+//   }
+//   if (kittenDesc2.includes(descrSearchText)) {
+//     listElement.innerHTML += kittenTwo;
+//   }
+//   if (kittenDesc3.includes(descrSearchText)) {
+//     listElement.innerHTML += kittenThree;
+//   }
+// };
+
+// buttonSearch.addEventListener("click", filterKitten);
+
+//ejemplo de un objeto con la información del primer gatito
+// const kittenData_1 = {
+//   image: "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg",
+//   name: "Anastacio",
+//   desc: "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+//   race: "British Shorthair",
+// };
+// const kittenData_2 = {
+//   image:
+//     "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg",
+//   name: "Fiona",
+//   desc: "Ruiseño,cariñoso, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+//   race: "British Shorthair",
+// };
+// const kittenData_3 = {
+//   image:
+//     "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg",
+//   name: "Cielo",
+//   desc: "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+//   race: "British Shorthair",
+// };
+
+// function renderKitten (kittenData) {
+//   console.log('kittenData.image');
+
+// }
+// renderKitten(kittenData_1);
 
 let listElement = document.querySelector(".js-listElement");
 
-const filterKitten = (event) => {
-  event.preventDefault();
-  if (kittenDesc1.includes(descrSearchText)) {
-    listElement.innerHTML += kittenOne;
-  }
-  if (kittenDesc2.includes(descrSearchText)) {
-    listElement.innerHTML += kittenTwo;
-  }
-  if (kittenDesc3.includes(descrSearchText)) {
-    listElement.innerHTML += kittenThree;
-  }
-};
+const kittenDataList = [
+  {
+    image: "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg",
+    name: "Anastacio",
+    desc: "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+    race: "British Shorthair",
+  },
+  {
+    image:
+      "https://static3.lasprovincias.es/www/multimedia/202112/12/media/cortadas/gatos-kb2-U160232243326NVC-1248x770@Las%20Provincias.jpg",
+    name: "Fiona",
+    desc: "Ruiseño,cariñoso, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+    race: "British Shorthair",
+  },
+  {
+    image:
+      "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg",
+    name: "Cielo",
+    desc: "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+    race: "British Shorthair",
+  },
+];
 
-buttonSearch.addEventListener("click", filterKitten);
+function renderKitten(perData) {
+  const html = `<li class="card">
 
+  <img class="card_img" src="${perData.image}" alt="gatito" />
+  <h3 class="card_title">${perData.name}</h3>
+  <h4 class="card_race">${perData.race}</h4>
+  <p class="card_description1 ">${perData.desc}
+  </p>
 
-//ejemplo de un objeto con la información del primer gatito
-const kittenData_1 = {
-  image: 'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg',
-  name: 'Anastacio',
-  desc: 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!',
-  race: 'British Shorthair',
-};
-const kittenData_2 = {
-  image: "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg",
-  name: 'Fiona',
-  desc: 'Ruiseño,cariñoso, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!',
-  race: 'British Shorthair',
-};
-const kittenData_3 = {
-  image: "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg",
-  name: 'Cielo',
-  desc: 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!',
-  race: 'British Shorthair',
-};
-
-function renderKitten (kittenData) {
-  console.log('kittenData.image');
-
+</li>`;
+  return html;
 }
-renderKitten(kittenData_1);
 
+let index = 0;
+let html = "";
+html += renderKitten(kittenDataList[index]);
 
+listElement.innerHTML = html;
+
+index++;
+html += renderKitten(kittenDataList[index]);
+listElement.innerHTML = html;
+index++;
+html += renderKitten(kittenDataList[index]);
+listElement.innerHTML = html;
